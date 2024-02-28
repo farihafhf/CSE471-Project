68725ecf80f9970cee44798f0359bd267f25ca9a
Maisha-Chowdhury <maisha.r.chowdhury1@gmail.com>
2024-02-29 01:09:58 +0600

    Changed DB settings from mysql to sqllite3

--- getitdone/settings.py
+++ getitdone/settings.py
@@ -79,15 +79,12 @@ WSGI_APPLICATION = 'getitdone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'getitdone',
-		'USER': 'root',
-		'PASSWORD': 'PaSSS',
-		'HOST':'localhost',
-		'PORT':'3306',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 STATICFILES_DIRS = [BASE_DIR, "static"]
 
 
