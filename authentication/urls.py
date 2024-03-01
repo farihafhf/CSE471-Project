6c3578b476341f7b95f42080f896b4a4de7215ad
farihafhf <160299298+farihafhf@users.noreply.github.com>
2024-03-02 03:14:19 +0600

    Update urls.py
    paths to view_profile and edit_profile added

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("signin",views.signin, name="signin"),
     path("signout",views.signout, name="signout"),
     path('dashboard', views.dashboard, name='dashboard'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('view_profile/', views.view_profile, name='view_profile'),
 ]
