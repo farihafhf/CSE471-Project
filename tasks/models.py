fe1622d4c17f27f0fea252f3d7183bd66eed2b85
Maisha-Chowdhury <maisha.r.chowdhury1@gmail.com>
2024-03-19 00:14:58 +0600

    fixed warning issue and minor change in index front end

--- tasks/models.py
+++ tasks/models.py
@@ -7,15 +7,6 @@ class Project(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)  
     created_at = models.DateTimeField(auto_now_add=True) 
 
-from django.db import models
-from django.contrib.auth.models import User
-
-class Project(models.Model):
-    project_id = models.AutoField(primary_key=True)  
-    project_name = models.CharField(max_length=100)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)  
-    created_at = models.DateTimeField(auto_now_add=True) 
-
 class Task(models.Model):
     task_id = models.AutoField(primary_key=True) 
     task_name = models.CharField(max_length=100)
